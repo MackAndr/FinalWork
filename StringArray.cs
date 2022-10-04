6a2711e4819e56c04427b4d26ece9ef6b71d9d10
Andrey Makushev <makandr63@yandex.ru>
2022-10-04 22:28:02 +0400

    Написал программу решения требуемой задачи

--- StringArray.cs
+++ StringArray.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите строки через пробел: ");
+string input = Console.ReadLine();
+var arrayOld = input.Split(" ");
+
+string[] arrayNew = new string[arrayOld.Length];
+int count = 0;
+
+for (int i = 0; i <arrayOld.Length; i++)
+{
+    if (arrayOld[i].Length <= 3)
+    {
+        count++;
+        arrayNew[count-1] = arrayOld[i];
+    }
+}
+
+Array.Resize(ref arrayNew, count);
+
+Console.WriteLine($"[{(String.Join(", ", arrayOld))}] --> [{(String.Join(", ", arrayNew))}]");
