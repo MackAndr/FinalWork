03d311b8c0bc5dcba4e28815c43db953bf107fbd
MackAndr <111796414+MackAndr@users.noreply.github.com>
2022-10-04 22:39:21 +0400

    Оформил программу в виде метода

--- StringArray.cs
+++ StringArray.cs
@@ -1,19 +1,23 @@
-﻿Console.Write("Введите строки через пробел: ");
-string input = Console.ReadLine();
-var arrayOld = input.Split(" ");
-
-string[] arrayNew = new string[arrayOld.Length];
-int count = 0;
-
-for (int i = 0; i <arrayOld.Length; i++)
+void StringArray()
 {
-    if (arrayOld[i].Length <= 3)
+    Console.Write("Введите строки через пробел: ");
+    string input = Console.ReadLine();
+    var arrayOld = input.Split(" ");
+
+    string[] arrayNew = new string[arrayOld.Length];
+    int count = 0;
+
+    for (int i = 0; i < arrayOld.Length; i++)
     {
-        count++;
-        arrayNew[count-1] = arrayOld[i];
+        if (arrayOld[i].Length <= 3)
+        {
+            count++;
+            arrayNew[count - 1] = arrayOld[i];
+        }
     }
+
+    Array.Resize(ref arrayNew, count);
+
+    Console.WriteLine($"[{(String.Join(", ", arrayOld))}] --> [{(String.Join(", ", arrayNew))}]");
 }
-
-Array.Resize(ref arrayNew, count);
-
-Console.WriteLine($"[{(String.Join(", ", arrayOld))}] --> [{(String.Join(", ", arrayNew))}]");
+StringArray();
